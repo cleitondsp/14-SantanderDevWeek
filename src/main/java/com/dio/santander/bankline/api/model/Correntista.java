0e5ba627949e25524a29473d7c968baf7f4a7916
cleitondsp <tirandoonda2014@gmail.com>
2022-05-06 19:59:26 -0300

    Adicionado JPA nas classes Correntista e Conta

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,11 +1,28 @@
 package com.dio.santander.bankline.api.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista") // criando uma tabela com o atributos abaixo
+
 public class Correntista {
 	
 	// atributos do correntista
-	
+		@Id														// essa sera chave primaria
+		@GeneratedValue(strategy = GenerationType.IDENTITY)		
 	private Integer id;
+	
+		@Column(length = 20)		// primeira coluna da tabela
 	private String cpf;
+		
+		@Column(length = 60)		// segunda coluna da tabela
 	private String nome;	
 	
 	public Integer getId() {
@@ -30,6 +47,7 @@ public class Correntista {
 	
 	// atributo da conta do correntista
 	
+		@Embedded		// significa que existe uma conexão com outra classe - "Conta"
 	private Conta conta; // Indica q esse correntista possui uma "Conta" "conta" que possui numero e saldo
 	
 	public Conta getConta() {
